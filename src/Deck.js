865db6572ca7a8d8e80893054bb3231b15e09a3f
Lisa Heffernan <lisaheffernan@outlook.com>
2023-01-04 17:38:50 +0000

    Deck tests passed - there are 52 cards in the deck and 13 of each suit

--- src/Deck.js
+++ src/Deck.js
@@ -1,11 +1,25 @@
+const Card = require("./Card");
+
 class Deck {
   constructor() {
     this.cards = [];
   }
 
-  initiateDeck() {}
+  initiateDeck() {
+    this.cards = this.populateSuit(["♣️", "♥️", "♠️", "♦️"]);
+  }
 
-  populateSuit() {}
+  populateSuit(suits) {
+    const cards = [];
+    suits.forEach((suit) => {
+      for (let i = 1; i <= 13; i += 1) {
+        const rank = i.toString();
+        const card = new Card(rank, suit);
+        cards.push(card);
+      }
+    });
+    return cards;
+  }
 }
 
 module.exports = Deck;
--- tests/Deck.unit.test.js
+++ tests/Deck.unit.test.js
@@ -20,4 +20,30 @@ describe("Deck", () => {
       expect(typeof deck.populateSuit).toBe("function");
     });
   });
+
+  describe("Deck has been populated correctly", () => {
+    let deck;
+    let unshuffledDeck;
+
+    beforeEach(() => {
+      deck = new Deck();
+      deck.initiateDeck();
+      unshuffledDeck = deck.cards;
+    });
+
+    it("Deck has 52 cards upon deck being populated", () => {
+      expect(unshuffledDeck.length).toEqual(52);
+    });
+
+    it("There are 13 of each suit", () => {
+      const suits = ["♣️", "♥️", "♠️", "♦️"];
+      const sortedSuits = suits.map((suit) =>
+        unshuffledDeck.filter((card) => card.cardSuit === suit)
+      );
+
+      sortedSuits.forEach((sortedSuit) =>
+        expect(sortedSuit.length).toEqual(13)
+      );
+    })
+  });
 });
