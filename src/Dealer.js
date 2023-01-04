a1f1e822285e623222a6030d672659ad18ac7fcb
Lisa Heffernan <lisaheffernan@outlook.com>
2023-01-04 18:01:49 +0000

    Dealer test passed - dealer is an object

---
+++ src/Dealer.js
@@ -0,0 +1,3 @@
+class Dealer {}
+
+module.exports = Dealer;
---
+++ tests/Dealer.unit.test.js
@@ -0,0 +1,27 @@
+const Dealer = require("../src/Dealer.js");
+
+describe("Dealer", () => {
+  describe("Dealer instantiation", () => {
+    let card;
+    let deck;
+    let dealer;
+
+    beforeEach(() => {
+      card = {
+        cardRank: 2,
+        cardSuit: "Mock",
+        cardValue: 2,
+        calculateValue: jest.fn(),
+      };
+      deck = {
+        cards: [card],
+        initiateDeck: jest.fn(),
+        populateSuit: jest.fn(),
+      };
+      dealer = new Dealer(deck);
+    });
+    it("Can be instantiated", () => {
+      expect(dealer).toBeInstanceOf(Object);
+    });
+  });
+});
