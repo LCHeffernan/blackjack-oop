de7be1d3f2076b409433fb95b4e1cebd82fbc3f4
Lisa Heffernan <lisaheffernan@outlook.com>
2023-01-04 19:42:43 +0000

    Hand test passed - playerHand is an empty array upon instantiation

--- src/Hand.js
+++ src/Hand.js
@@ -1,5 +1,7 @@
 class Hand {
-
+  constructor() {
+    this.playerHand = [];
+  }
 }
 
 module.exports = Hand;
--- tests/Hand.unit.test.js
+++ tests/Hand.unit.test.js
@@ -2,9 +2,17 @@ const Hand = require("../src/Hand.js");
 
 describe("Hand", () => {
   describe("hand instantiation", () => {
+    let hand;
+
+    beforeEach(() => {
+      hand = new Hand();
+    });
     it("can be instantiated", () => {
-      const hand = new Hand();
       expect(hand).toBeInstanceOf(Object);
+    });
+
+    it("playerHand is an empty array", () => {
+      expect(hand.playerHand.length).toEqual(0);
     });
   });
 });
